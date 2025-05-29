b44cf3e88feed59b32314283cc75043929a6c449
N0Namesir <rivasmejiaeduardo@gmail.com>
2025-05-29 02:15:10 -0600

    basico

---
+++ config/conexion.php
@@ -0,0 +1,23 @@
+<?php
+// Tu código de conexión aquí
+$host = 'localhost';
+$db   = 'tecnoMundo';
+$user = 'root';
+$pass = 'sqlpass';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+?>
+
